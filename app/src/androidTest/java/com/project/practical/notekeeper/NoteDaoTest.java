2882103f9ae40a4154314e2fe455b9ff234bbffe
saeed <ghfarisaeed1@gmail.com>
2023-09-10 03:15:35 -0600

    create test util class in bouth of test and android test

---
+++ app/src/androidTest/java/com/project/practical/notekeeper/NoteDaoTest.java
@@ -0,0 +1,17 @@
+package com.project.practical.notekeeper;
+
+import com.project.practical.notekeeper.modulse.Note;
+import com.project.practical.notekeeper.util.TestUtil;
+
+import org.junit.Test;
+
+public class NoteDaoTest extends NoteDatabaseTest{
+
+
+
+    @Test
+    public void insertReadDeleteData() throws Exception{
+        //Note note=new Note(TestUtil.)
+
+    }
+}
